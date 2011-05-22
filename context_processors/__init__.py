d73d7754958b60248c5ec2c070158456d41064a0
Brant Steen <brant.steen@gmail.com>
2011-05-22 10:11:39 -0400

    OCD vs Pylint

--- context_processors/__init__.py
+++ context_processors/__init__.py
@@ -9,10 +9,12 @@ or use mini_js/mini_css separately
 
 from mediabrute.context_processors.handlers import minify_css, minify_js
 
-def mini_media(*args, **kwargs):
+def mini_media(request):
     """
     Context processor to expose {{ MINI_JS }} and {{ MINI_CSS }}
     """
+    minis = {}
+    
     minis.update(mini_css())
     minis.update(mini_js())
     return minis
