b6d2379980e7791c8781d8a1d1f8297e47a86334
Tim White <tim@cyface.com>
2011-07-23 14:05:43 -0600

    Created mediabrute subdir so that packages line up inside egg.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "mediabrute",
+    name = "django-mediabrute",
     version = ".1",
     url = 'http://github.com/Brant/django-mediabrute',
     license = 'GPL',
