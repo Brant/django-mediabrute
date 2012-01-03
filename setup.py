795d56662c5ee0ae05c59ff10ba3c10014f4a697
Brant Steen <brant.steen@gmail.com>
2012-01-02 22:19:11 -0500

    Setup script to move static stuff

--- setup.py
+++ setup.py
@@ -1,5 +1,45 @@
 import os
+import sys
+
 from setuptools import setup, find_packages
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    
+    Taken from Django's setup.py
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# Taken from Django's setup.py
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != "":
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk("noodles"):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])    
+
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -15,8 +55,11 @@ setup(
     author = 'Brant Steen',
     author_email = 'brant.steen@gmail.com',
 
-    packages = find_packages('.'),
-    package_dir = {'': '.'},
+    packages = packages,
+    data_files = data_files,
+    zip_safe = False,
+    
+#    package_dir = {'': '.'},
 
     install_requires = ['setuptools', 'django'],
 
