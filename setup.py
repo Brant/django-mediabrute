3a5407058345c38f59ed4d848aa652ff097e92ee
Brant Steen <brant.steen@gmail.com>
2012-06-29 12:08:15 -0400

    Lets give this a whirl

--- setup.py
+++ setup.py
@@ -1,48 +1,4 @@
-import os
-import sys
-
 from setuptools import setup, find_packages
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    
-    Taken from Django's setup.py
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-# Taken from Django's setup.py
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != "":
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk("mediabrute"):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])    
-
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-mediabrute",
@@ -50,16 +6,14 @@ setup(
     url = 'http://github.com/Brant/django-mediabrute',
     license = 'GPL',
     description = "Django MediaBute Compresses and Consolidates CSS and JS Files for Django Applications",
-    long_description = read('README.md'),
+    long_description = open('README.rst').read(),
 
     author = 'Brant Steen',
     author_email = 'brant.steen@gmail.com',
 
-    packages = packages,
-    data_files = data_files,
+    packages = find_packages(exclude=('socialprofile_demo', 'tests', )),
+    include_package_data = True,
     zip_safe = False,
-    
-#    package_dir = {'': '.'},
 
     install_requires = ['setuptools', 'django'],
 
