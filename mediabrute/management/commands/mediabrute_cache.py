4c6f441e3da48c2157496aaeed623f80bd14f6c4
Brant Steen <brant.steen@gmail.com>
2012-09-18 14:45:43 -0400

    writing some files

--- mediabrute/management/commands/mediabrute_cache.py
+++ mediabrute/management/commands/mediabrute_cache.py
@@ -15,6 +15,13 @@ class Command(BaseCommand):
         """
         Cache Static Files
         """
-        handlers.minify_js()
-        handlers.minify_css()
+        js_url = handlers.minify_js()
+        js_file = open(os.path.join(generate_cache_dir("js"), "mediabrute_usefile"))
+        js_file.write(js_url)
+        js_file.close()
+        
+        css_url = handlers.minify_css()
+        css_file = open(os.path.join(generate_cache_dir("css"), "mediabrute_usefile"))
+        css_file.write(css_url)
+        css_file.close()
         
