0f90fc4edb1a50f288e584ce84c73d121df834b2
Brant Steen <brant.steen@gmail.com>
2012-09-18 14:50:03 -0400

    pluralizze!

--- mediabrute/management/commands/mediabrute_cache.py
+++ mediabrute/management/commands/mediabrute_cache.py
@@ -17,13 +17,13 @@ class Command(BaseCommand):
         """
         Cache Static Files
         """
-        js_url = handlers.minify_js()
+        js_urls = handlers.minify_js()
         js_file = open(os.path.join(generate_cache_dir("js"), "mediabrute_usefile"))
-        js_file.write(js_url)
+        js_file.write(["%s\n" % url for url in js_urls])
         js_file.close()
         
-        css_url = handlers.minify_css()
+        css_urls = handlers.minify_css()
         css_file = open(os.path.join(generate_cache_dir("css"), "mediabrute_usefile"))
-        css_file.write(css_url)
-        css_file.close()
+        css_file.write(css_urls)
+        css_file.close(["%s\n" % url for url in css_urls])
         
